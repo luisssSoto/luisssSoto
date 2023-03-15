26f3d88935d4344b6fbb66f6a223ea8bb65bd6ab
luisSoto <alejandrosotoserafin@gmail.com>
2023-03-15 17:34:47 -0600

    exercisesUpdate

---
+++ ejerciciosLogica/logica-programacion-8/ejercicioLogica8/src/ejercicioLogica8/numerosPrimos/Test.java
@@ -0,0 +1,14 @@
+package ejercicioLogica8.numerosPrimos;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0;
+		while (x <= 10) {
+			System.out.println(x);
+			x++;
+		}
+	}
+
+}
